f3db092e120a36979d049ad97e385924ae49a9d9
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-28 01:07:21 +0530

    Add files via upload

---
+++ INTRODUCTION/12. Line Counting.c
@@ -0,0 +1,15 @@
+// Counting Input Lines
+#include <stdio.h>
+
+int main(void) {
+	int c, nl;
+
+	nl = 0;
+	while((c = getchar()) != EOF) {
+		if(c == '\n') {
+			++nl;
+        }
+    }
+	printf("%d\n", nl);
+	return 0;
+}
