c3f15c6942b7a9646d00f44b7a9c6d2b1189c858
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-28 15:17:57 +0530

    Update 6. squeeze.c

--- TYPES, OPERATORS & EXPRESSIONS/6. squeeze.c
+++ TYPES, OPERATORS & EXPRESSIONS/6. squeeze.c
@@ -1,21 +1,18 @@
+// squeeze implementation
 #include <stdio.h>
 
 void squeeze(char[], int);
 
-main()
-{
+int main(void) {
 	char s[] = "test";
 	squeeze(s, 't');
 	printf("%s\n", s);
 }
 
-void squeeze(char s[], int c)
-{
+void squeeze(char s[], int c) {
 	int i ,j;
-
-	for (i = j = 0; s[i] != '\0'; i++)
-		if (s[i] != c)
+	for(i = j = 0; s[i] != '\0'; i++)
+		if(s[i] != c)
 			s[j++] = s[i];
-
 	s[j] = '\0';
 }
