f6d1ddc32181b6de3b7012c7d2cca2316f4e88ec
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-28 08:16:36 +0530

    Add files via upload

---
+++ INTRODUCTION/14. Replace Multispaces.c
@@ -0,0 +1,14 @@
+// Replace Multispaces
+#include <stdio.h>
+
+int main(void) {
+	int c, pre;
+
+	while((c = getchar()) != EOF) {
+		if(c != ' ' || pre != ' ') {
+			putchar(c);
+		}
+		pre = c;
+	}
+	return 0;
+}
