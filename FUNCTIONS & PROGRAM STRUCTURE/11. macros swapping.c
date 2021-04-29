6ae19e71dd5ce1112400b4f1fe70d62c18dd48b4
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-29 20:14:19 +0530

    Add files via upload

---
+++ FUNCTIONS & PROGRAM STRUCTURE/11. macros swapping.c
@@ -0,0 +1,19 @@
+// Swapping of macros 
+#include <stdio.h>
+
+#define swap(t, x, y) { t temp = x; x = y; y = temp; }
+
+int main(void) {
+	int x, y;
+	x = 1;
+	y = 2;
+	swap(int, x, y);
+	printf("%d, %d\n", x, y);
+
+	double a, b;
+	a = 1.2;
+	b = -3.9;
+	swap(double, a, b);
+	printf("%g, %g\n", a, b);
+	return 0;
+}
