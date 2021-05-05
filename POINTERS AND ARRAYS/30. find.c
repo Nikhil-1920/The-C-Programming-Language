edc626467907d4a49f376a240cc4745b9975a686
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-05-05 23:15:54 +0530

    Add files via upload

---
+++ POINTERS AND ARRAYS/30. find.c
@@ -0,0 +1,24 @@
+// find() implementation
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int mygetline(char *line, int max);
+
+int main(int argc, char *argv[]) {
+	char line[MAXLINE];
+	int found = 0;
+
+	if(argc != 2)
+		printf("Usage: find pattern\n");
+
+	else
+		while(mygetline(line, MAXLINE) > 0)
+			if(strstr(line, argv[1]) != NULL) {
+				printf("%s", line);
+				found++;
+			}
+	return found;
+}
+
