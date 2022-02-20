62b0879b715266fe5119c0c2f1a264574741b264
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-02-20 16:01:28 +0530

    Update 2. Fahr-Cels Table.c

--- INTRODUCTION/2. Fahr-Cels Table.c
+++ INTRODUCTION/2. Fahr-Cels Table.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 
 int main(void) {
+	
 	int fahr, cels;
 	int lower, upper;
 	int step;
 
 	lower = 0;	    /* lower limit of temperature table */
 	upper = 300;	    /* upper limit of temperature table */
-	step  = 20;	    /* size of step*/
-
+	step  = 20;	    // size of step
+	
         fahr = lower;
         
         printf("Fahr\tCels\n");
 	while(fahr <= upper) {
-		cels = 5 * (fahr-32) / 9;
+		cels = 5 * (fahr - 32) / 9;
 		printf("%3d\t%3d\n", fahr, cels);
 		fahr = fahr + step;
 	}
