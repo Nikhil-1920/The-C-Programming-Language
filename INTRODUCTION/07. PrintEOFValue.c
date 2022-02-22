8c1b5bb7aa1e6c2034ac78cea51db7112d69cd70
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-02-22 19:23:16 +0530

    Update and rename 9. Print EOF.c to 07. PrintEOFValue.c

---
+++ INTRODUCTION/07. PrintEOFValue.c
@@ -0,0 +1,13 @@
+/*  Q.7. Write a program to print the value of EOF. */
+
+// Author: Nikhil Singh
+// Last Modified: 21/02/2022
+
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("The constant value of EOF:> %d\n", EOF);
+    return 0;
+}
--- INTRODUCTION/9. Print EOF.c
+++
@@ -1,7 +0,0 @@
-// Printing EOF Value
-#include <stdio.h>
-
-int main(void) {
-	printf("%d\n", EOF);
-	return 0;
-}
