5753b10b288f56b0a381089e07baed5ec086fbf6
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-02-23 09:26:21 +0530

    Update and rename 7. File Copying-2.c to 09. CopyInputToOutput-2.c

---
+++ INTRODUCTION/09. CopyInputToOutput-2.c
@@ -0,0 +1,27 @@
+/*  Q.9. Write a program to copy its input to its output, 
+    replacing each string of one or more blanks by a single 
+    blank.
+*/
+
+// Author: Nikhil Singh
+// Last Modified: 20/02/2022
+
+
+#include <stdio.h>
+
+int main(void) {
+
+    char current, previous = EOF;
+
+    while((current = getchar()) != EOF) {
+
+        if(current == ' ') {
+            if(previous != current) putchar(current);
+        }
+        if(current != ' ') putchar(current);
+
+        // Following current character
+        previous = current;
+    }
+    return 0;
+}
