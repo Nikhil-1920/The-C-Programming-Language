128842f62b7e8c32e12b2aeaac509554bdeca528
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-02-27 11:50:56 +0530

    Update and rename 11. Character Counting-2.c to 11. TestingWordCount.c

---
+++ INTRODUCTION/11. TestingWordCount.c
@@ -0,0 +1,91 @@
+/*  Q.11. How would you test the word count program? What kinds 
+    of input are most likely to uncover bugs if there are any?
+*/
+
+// Author: Nikhil Singh
+// Last Modified: 22/02/2022
+
+
+#include <stdio.h>
+#include <assert.h>
+
+
+int main(void) {
+
+    FILE *fptr = NULL;
+
+    unsigned long i = 0;
+    static char *spaces = " \f\t\v";
+    static char *alphab = "abcdefghijklmnopqrstuvwxyz";
+    static char *alpha5 = "a b c d e f g h i j k l m "
+                          "n o p q r s t u v w x y z "
+                          "a b c d e f g h i j k l m "
+                          "n o p q r s t u v w x y z "
+                          "a b c d e f g h i j k l m "
+                          "n\n";
+
+
+    // Input file containing zero words
+    fptr = fopen("test1", "w");
+    assert(fptr != NULL);
+    fclose(fptr);
+
+
+    // Input file containing one huge word without newline
+    fptr = fopen("test2", "w");
+    assert(fptr != NULL);
+    for(i = 0; i < (66000ul / 26 + 1); i++)
+        fputs(alphab, fptr);
+    
+    fclose(fptr);
+
+
+    // Input file containing different kinds of spaces
+    fptr = fopen("test3", "w");
+    assert(fptr != NULL);
+    for(i = 0; i < (66000ul / 4 + 1); i++)
+        fputs(spaces, fptr);
+    
+    fclose(fptr);
+
+
+    // Input file containing words and different sorts of spaces
+    fptr = fopen("test4", "w");
+    assert(fptr != NULL);
+    fputs("sequoia", fptr);
+    
+    for(i = 0; i < (66000ul / 26 + 1); i++)
+        fputs(spaces, fptr);
+    
+    fputs("sequoia", fptr);
+    fclose(fptr);
+
+
+    // Input files contains 66000 newlines
+    fptr = fopen("test5", "w");
+    assert(fptr != NULL);
+    for(i = 0; i < 66000ul; i++)
+        fputs("\n", fptr);
+    
+    fclose(fptr);
+
+
+    // Input files contains 66000 single letter words
+    fptr = fopen("test6", "w");
+    assert(fptr != NULL);
+    for(i = 0; i < 1000; i++)
+        fputs(alpha5, fptr);
+    
+    fclose(fptr);
+
+
+    // Input files contains 66000 words without newline
+    fptr = fopen("test7", "w");
+    assert(fptr != NULL);
+    for(i = 0; i < 66000ul; i++)  
+        fputs("sequoia", fptr);
+
+    fclose(fptr);
+
+    return 0;
+}
