ed2c987fa50582b5b86bae9d6d670a2bc7ba567b
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-10-16 10:41:06 +0530

    Update and rename 10. ReplaceTabsBackspacesBackslashes.c to Exercise-10.c

--- INTRODUCTION/Exercise-10.c
+++ INTRODUCTION/Exercise-10.c
@@ -1,6 +1,5 @@
-/*  Write a program to copy its input to its output, replacing each 
-    tab by \t, each backspaces by \b, and each backslash by \\. This 
-    makes tabs and backspaces visible in an unambiguous way.
+/*  Write a program to copy its input to its output, replacing each tab by \t, each backspaces by \b, and 
+    each backslash by \\. This makes tabs and backspaces visible in an unambiguous way.
 */
 
 // Author: Nikhil Singh
@@ -12,22 +11,27 @@
 int main(void) {
 
     int ch, isModified = 0;
+    
     while((ch = getchar()) != EOF) {
 
         isModified = 0;
+        
         if(ch == '\t') {
+            
             putchar('\\');
             putchar('t');
             isModified = 1;
         }
         
         if(ch == '\b') {
+            
             putchar('\\');
             putchar('b');
             isModified = 1;
         }
 
         if(ch == '\\') {
+            
             putchar('\\');
             putchar('\\');
             isModified = 1;
@@ -35,5 +39,6 @@ int main(void) {
 
         if(isModified == 0) putchar(ch);
     }
+    
     return 0;
 }
