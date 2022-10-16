91ab67cf1ecab84b6a2191ec38247dd6ee28ef59
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2022-10-16 10:26:24 +0530

    Update and rename 02. EscapeSequences.c to Exercise-2.c

--- INTRODUCTION/Exercise-2.c
+++ INTRODUCTION/Exercise-2.c
@@ -1,5 +1,4 @@
-/*  Q.2. Experiment to find out what happens when 
-    printf 's argument string contains \c, where c 
+/*  Experiment to find out what happens when printf's argument string contains \c, where c 
     is some character not listed above.
 */
 
